5a72d5c503843cf130b06d468d64a880de827bf2
Dave Syer <dsyer@vmware.com>
2011-01-07 16:46:41 +0000

    Add eager init to factory bean

--- magott-springbatch-poc/src/main/java/no/magott/spring/batch/stepscope/CountingFactoryBean.java
+++ magott-springbatch-poc/src/main/java/no/magott/spring/batch/stepscope/CountingFactoryBean.java
@@ -3,15 +3,21 @@ package no.magott.spring.batch.stepscope;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class CountingFactoryBean implements FactoryBean<Message> {
+public class CountingFactoryBean implements FactoryBean<Message>, InitializingBean {
 
 	private final static AtomicInteger invocationCount = new AtomicInteger();
 	private String message;
+	private Message instance;
+	
+	public void afterPropertiesSet() throws Exception {
+		int count = invocationCount.incrementAndGet();
+		instance = new SimpleMessage(message,count);
+	}
 
 	public Message getObject() throws Exception {
-		int count = invocationCount.incrementAndGet();
-		return new SimpleMessage(message,count);
+		return instance;
 	}
 
 	public Class<?> getObjectType() {
