5757903588ec0da58dd2908ea3f892bcc9e78254
Morten Andersen-Gott <morten.andersengott@gmail.com>
2011-01-12 10:38:16 +0100

    First simple test of spring integration

---
+++ magott-springintegration-poc/src/test/java/no/magott/spring/integration/FilePollingTest.java
@@ -0,0 +1,22 @@
+package no.magott.spring.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:file-polling.xml")
+public class FilePollingTest {
+
+	@Value("${java.io.tmpdir}")
+	private String tempDir;
+	
+	@Test
+	public void printTempDir() throws Exception{
+		System.out.println(tempDir);
+		Thread.sleep(10000);
+	}
+	
+}
---
+++ magott-springintegration-poc/src/test/java/no/magott/spring/integration/SysOutService.java
@@ -0,0 +1,9 @@
+package no.magott.spring.integration;
+
+public class SysOutService {
+
+	public void print(String value){
+		System.out.println("Fooooooo");
+		System.out.println(value);
+	}
+}
